92d569e3272222f8a3d9a202619b36e528c61138
Daniel Brännvall <daniel@brannvall.net>
2022-12-14 12:05:06 +0100

    Formatting

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main
-  ( main
-  ) where
+    ( main
+    ) where
 
 import           Control.Monad                  ( forM_ )
 import           Data.Map                       ( Map )
@@ -40,22 +40,22 @@ import           Day14
 
 days :: Map Int ([String] -> (Maybe String, Maybe String))
 days = M.fromList
-  [ (1, Day01.solve)
-  , (2, Day02.solve)
-  , (3, Day03.solve)
-  , (4, Day04.solve)
-  , (5, Day05.solve)
-  , (6, Day06.solve)
-  , (7, Day07.solve)
-  , (8, Day08.solve)
-  , (9, Day09.solve)
-  , (10, Day10.solve)
-  , (11, Day11.solve)
-  , (12, Day12.solve)
-  , (13, Day13.solve)
-  , (14, Day14.solve)
+    [ (1 , Day01.solve)
+    , (2 , Day02.solve)
+    , (3 , Day03.solve)
+    , (4 , Day04.solve)
+    , (5 , Day05.solve)
+    , (6 , Day06.solve)
+    , (7 , Day07.solve)
+    , (8 , Day08.solve)
+    , (9 , Day09.solve)
+    , (10, Day10.solve)
+    , (11, Day11.solve)
+    , (12, Day12.solve)
+    , (13, Day13.solve)
+    , (14, Day14.solve)
   {-- insert map entry here --}
-  ]
+    ]
 
 newtype Arguments = Arguments
   { dayNumber :: String
@@ -63,26 +63,26 @@ newtype Arguments = Arguments
 
 arguments :: Parser Arguments
 arguments = Arguments <$> argument
-  str
-  (metavar "DAY" <> help "Number of the day to run, or \"all\" for all days")
+    str
+    (metavar "DAY" <> help "Number of the day to run, or \"all\" for all days")
 
 day :: Int -> IO ()
 day n = do
-  let daynum = printf "%02d" n
-  let mf = fromMaybe (const (Nothing, Nothing)) (days M.!? n)
-  xs <- input $ "data/input" ++ daynum ++ ".txt"
-  let (a, b) = mf xs
-  putStrLn $ daynum ++ "a: " ++ fromMaybe "-" a
-  putStrLn $ daynum ++ "b: " ++ fromMaybe "-" b
+    let daynum = printf "%02d" n
+    let mf = fromMaybe (const (Nothing, Nothing)) (days M.!? n)
+    xs <- input $ "data/input" ++ daynum ++ ".txt"
+    let (a, b) = mf xs
+    putStrLn $ daynum ++ "a: " ++ fromMaybe "-" a
+    putStrLn $ daynum ++ "b: " ++ fromMaybe "-" b
 
 runDay :: Arguments -> IO ()
 runDay arg = case dayNumber arg of
-  "all" -> forM_ [1 .. 25] day
-  dn    -> maybe (putStrLn "Illegal day number") day (readMaybe dn)
+    "all" -> forM_ [1 .. 25] day
+    dn    -> maybe (putStrLn "Illegal day number") day (readMaybe dn)
 
 main :: IO ()
 main = execParser args >>= runDay
- where
-  args = info
-    (helper <*> arguments)
-    (fullDesc <> header "AoC2022 - run solutions for Advent of Code 2022")
+  where
+    args = info
+        (helper <*> arguments)
+        (fullDesc <> header "AoC2022 - run solutions for Advent of Code 2022")
